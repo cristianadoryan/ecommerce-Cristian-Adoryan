c7efe2072a92c523999956d83d125f3585b84839
cristian adoryan <cristianadoryan@gmail.com>
2025-03-17 12:52:24 -0300

    agregando estilos en rama

--- src/components/common/productCard/ProductCard.jsx
+++ src/components/common/productCard/ProductCard.jsx
@@ -17,7 +17,13 @@ const ProductCard = ({ price, title, stock, id, imageUrl }) => {
       </h2>
 
       <Link to={`/itemDetail/${id}`} className="product-link">
-        <Button variant="outlined" className="gothic-button" fullWidth>
+        <Button
+          variant="outlined"
+          sx={{
+            borderColor: "green", // Cambia el borde a verde
+            color: "green", // Cambia el color del texto a verde (opcional)
+          }}
+        >
           Ver detalle
         </Button>
       </Link>
